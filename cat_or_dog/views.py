f15efc7603bb6ac0e9744e444d6b21d80de7c7b8
Michu0704 <michu07048@wp.pl>
2021-02-11 21:00:33 +0100

    login and logged out templates

--- cat_or_dog/views.py
+++ cat_or_dog/views.py
@@ -19,7 +19,7 @@ def signup(request):
             return redirect('index')
     else:
         form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'registration/signup.html', {'form': form})
 
 
 def index(request):
