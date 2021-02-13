fcbac22bbb36f68841839f855495988ec079b807
Michu0704 <michu07048@wp.pl>
2021-02-13 22:08:27 +0100

    machine learning works with paths from pc

--- cat_or_dog/forms.py
+++ cat_or_dog/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from .models import *
+
+
 User = get_user_model()
 
 
@@ -8,3 +11,4 @@ class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, required=True, help_text='Required. Inform a valid email address.')
     password1 = forms.CharField(label="Haslo", strip=False, widget=forms.PasswordInput)
     password2 = forms.CharField(label="Powtorz haslo", strip=False, widget=forms.PasswordInput)
+
--- cat_or_dog/views.py
+++ cat_or_dog/views.py
@@ -1,10 +1,15 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
-from django.views import generic
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
 from django.contrib.auth import login, authenticate
 from .forms import SignUpForm
+import sys
+import numpy as np
+from .forms import *
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.preprocessing import image
+from django.http import HttpResponseRedirect
+
+model = load_model('/home/michu/Pulpit/my_model-cat-dog.h5')
 
 
 def signup(request):
@@ -23,4 +28,29 @@ def signup(request):
 
 
 def index(request):
-    return render(request, 'index.html')
+    #return render(request, 'index.html', {'form': form})
+    f = '/home/michu/Obrazy/1.jpeg'
+    cls_list = ['cats', 'dogs']
+
+    # zaladowanie wytrenowanego modelu
+    #net = load_model('./my_model-cat-dog.h5')
+
+    # loop through all files and make predictions
+    output = {}
+    # Pobranie zdjecia w formacie 224px x 224px
+    img = image.load_img(f, target_size=(224,224))
+    if img is None:
+        print("blad")
+        sys.exit(1)
+    ## Przetwarzanie obrazu na wstepie, tak jak resnet50
+    x = image.img_to_array(img)
+    x = preprocess_input(x)
+    x = np.expand_dims(x, axis=0)
+    # Przewidywanie klasy na podstawie wytrenowanego modelu
+    pred = model.predict(x)[0]
+    top_inds = pred.argsort()[::-1][:5]
+    prediction = {}
+    for i in top_inds:
+        prediction[i] = '{}'.format(cls_list[i])
+        print('{:.3f}{}'.format(pred[i], cls_list[i]))
+    return render(request, 'index.html', {'pred': prediction})
